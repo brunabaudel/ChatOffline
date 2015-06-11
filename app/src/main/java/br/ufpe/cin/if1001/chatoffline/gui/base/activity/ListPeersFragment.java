fb87dcc298b9f00d14e1c76ca7e11dd231ca72a8
brunabaudel <brubaudel@gmail.com>
2015-06-11 10:26:20 -0300

    Código base da gui.

---
+++ app/src/main/java/br/ufpe/cin/if1001/chatoffline/gui/base/activity/ListPeersFragment.java
@@ -0,0 +1,42 @@
+package br.ufpe.cin.if1001.chatoffline.gui.base.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.ufpe.cin.if1001.chatoffline.R;
+
+
+public class ListPeersFragment extends Fragment {
+
+    public ListPeersFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_list_peers, container, false);
+
+
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
---
+++ app/src/main/java/br/ufpe/cin/if1001/chatoffline/gui/base/navdrawer/NavDrawerItem.java
@@ -0,0 +1,18 @@
+package br.ufpe.cin.if1001.chatoffline.gui.base.navdrawer;
+
+public class NavDrawerItem {
+
+    private String title;
+
+    public NavDrawerItem() {
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}

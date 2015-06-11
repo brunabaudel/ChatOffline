ac8bb7ab43658d69bc5e455b6781fce67c824d7d
brunabaudel <brubaudel@gmail.com>
2015-06-11 11:41:28 -0300

    Fragment da Lista de Peers.

--- app/src/main/java/br/ufpe/cin/if1001/chatoffline/gui/base/activity/ListPeersFragment.java
+++ app/src/main/java/br/ufpe/cin/if1001/chatoffline/gui/base/activity/ListPeersFragment.java
@@ -1,42 +1,65 @@
 package br.ufpe.cin.if1001.chatoffline.gui.base.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.ufpe.cin.if1001.chatoffline.R;
+import br.ufpe.cin.if1001.chatoffline.gui.contact.ListPeersAdapter;
+import br.ufpe.cin.if1001.chatoffline.gui.contact.Peer;
 
 
 public class ListPeersFragment extends Fragment {
 
+    private static String TAG = ListPeersFragment.class.getSimpleName();
+
+    private RecyclerView recyclerView;
+    private ListPeersAdapter adapter;
+    private static String[] peers = null;
+
     public ListPeersFragment() {
+
+    }
+
+    public static List<Peer> getData() {
+        List<Peer> data = new ArrayList<>();
+
+        for (String p : peers) {
+            Peer peer = new Peer();
+            peer.setName(p);
+            data.add(peer);
+        }
+
+        return data;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        peers = getActivity().getResources().getStringArray(R.array.list_peers_labels);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_list_peers, container, false);
 
+        View layout = inflater.inflate(R.layout.fragment_list_peers, container, false);
+        recyclerView = (RecyclerView) layout.findViewById(R.id.list_peers);
 
-        return rootView;
+        adapter = new ListPeersAdapter(getActivity(), getData());
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return layout;
     }
 
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-    }
 }
---
+++ app/src/main/java/br/ufpe/cin/if1001/chatoffline/gui/contact/ListPeersAdapter.java
@@ -0,0 +1,59 @@
+package br.ufpe.cin.if1001.chatoffline.gui.contact;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.List;
+
+import br.ufpe.cin.if1001.chatoffline.R;
+
+
+public class ListPeersAdapter extends RecyclerView.Adapter<ListPeersAdapter.ListPeersViewHolder> {
+
+    List<Peer> items = Collections.emptyList();
+    private LayoutInflater inflater;
+    private Context context;
+
+    public ListPeersAdapter(Context context, List<Peer> items) {
+        this.context = context;
+        inflater = LayoutInflater.from(context);
+        this.items = items;
+    }
+
+    public void delete(int position) {
+        items.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    @Override
+    public ListPeersViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.list_peer_row, parent, false);
+        ListPeersViewHolder listPeersHolder = new ListPeersViewHolder(view);
+        return listPeersHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ListPeersViewHolder listPeersHolder, int position) {
+        Peer current = items.get(position);
+        listPeersHolder.name.setText(current.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    class ListPeersViewHolder extends RecyclerView.ViewHolder {
+        TextView name;
+
+        public ListPeersViewHolder(View view) {
+            super(view);
+            name = (TextView) view.findViewById(R.id.name);
+        }
+    }
+}
